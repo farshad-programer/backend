c14581910a3f3dae582f69846530d1c8fc7eeebf
farshad bagheri <farshad65bagheri05@gmail.com>
2023-05-08 14:01:21 -0700

    Error-control

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 import express from "express";
 const app = express();
+import * as dotenv from "dotenv";
+dotenv.config();
 import debug from "debug";
 debug("app:main");
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import config from "config";
 import winston from "winston";
+import AppError from "./src/ErrorControls/AppError.js";
 import credentials from "./src/middlewares/credential.js";
 import configApp from "./startup/config.js";
 import db from "./startup/db.js";
 import logging from "./startup/logging.js";
 import router from "./src/routes/index.js";
 import cors from "cors";
+import globalErrorHandler from "./src/ErrorControls/errorController.js";
 
 logging();
 configApp(app, express, cookieParser, cors, credentials);
 db();
 app.use("/api", router);
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+app.use(globalErrorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
